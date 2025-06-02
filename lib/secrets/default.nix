87af121c2bfa9437c42b4bb665dbc3248d783eb1
sobte <i@sobte.me>
2025-06-03 06:56:05 +0800

    feat: update secrets configuration to disable by default

--- lib/secrets/default.nix
+++ lib/secrets/default.nix
@@ -1,7 +1,8 @@
 { inputs, ... }:
 {
   secrets = {
-    enable = true;
+    # if you need to use secrets, set to true
+    enable = false;
     # Import `my-secrets` in flake.nix
     # Here, enable and set the secret repository.
     # If you don’t need to use secrets, you can remove them yourself.
